a60f711d7707313bc09b641f808df036512d9f59
TEST <yogeshtiwari6260@gmail.com>
2023-03-10 17:38:37 +0530

    Deliveroo-Clone Completed

---
+++ client/screens/HomeScreen.js
@@ -0,0 +1,106 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  Platform,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  ChevronDownIcon,
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
+import sanityClient from "../sanity";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "featured"] {
+        ...,
+        restaurants[]->{
+          ...,
+          dishes[]->
+        }
+      }`
+      )
+      .then((data) => {
+        setFeaturedCategories(data);
+      });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container} className="pt-5">
+      {/* Header  */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1 justify-center">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <View className="flex-row items-center space-x-1">
+            <Text className="font-bold text-xl">Current Location</Text>
+            <ChevronDownIcon size={22} color="#00CCBB" />
+          </View>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* Search  */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row items-center space-x-2 flex-1 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={22} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+      {/* Body  */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{ paddingBottom: 150 }}
+      >
+        {/* Categories  */}
+        <Categories />
+        {/* Featured Rows  */}
+
+        {featuredCategories?.map((category) => (
+          <FeaturedRow
+            key={category._id}
+            id={category._id}
+            title={category.name}
+            description={category.short_description}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Platform.OS === "android" ? 30 : 0,
+    backgroundColor: "#fff",
+  },
+});
+
+export default HomeScreen;
---
+++ server/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '49k8c9zg',
+    dataset: 'production'
+  }
+})
